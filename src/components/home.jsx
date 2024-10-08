4a0a6949510402cd214052d0ef4b0ebc002202d4
toga1755 <toga.1411@icloud.com>
2024-10-08 18:27:08 +0900

    あ

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -19,7 +19,7 @@ export default function HomePage() {
                 <Box sx={{ height: "40%" }}>
                     <TopNotice />
                 </Box>
-                <Box sx={{ height: "50%" }}>
+                <Box sx={{ height: "100%" }}>
                     <TopMenu />
                 </Box>
             </Box>
